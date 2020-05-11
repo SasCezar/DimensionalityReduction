c2a6e6c310e27020b7f54fa687bbc5a56c06a5e9
Cezar Sas <cezar.sas@gmail.com>
2020-05-11 16:50:40 +0200

    Fixed and generalized

--- dimred/models/neural.py
+++ dimred/models/neural.py
@@ -1,25 +1,48 @@
+import copy
+from typing import Iterable, Callable
+
 import torch
 from torch import nn
 
 
-def _init_modules(hidden_sizes, activation_function, reverse):
-    layers = []
-    if reverse:
-        hidden_sizes = list(reversed(hidden_sizes))
-    for i, l_in, l_out in enumerate(zip(hidden_sizes, hidden_sizes[1:])):
-        layers.append(nn.Linear(l_in, l_out))
-        layers.append(activation_function[i])
-    layers.append(activation_function[-1])
-    return layers
-
-
-class AE(nn.Module):
-    def __init__(self, hidden_sizes, activation_functions):
+class AbstractAE(nn.Module):
+    def __init__(self, hidden_sizes: Iterable, activation_function: Callable = nn.ReLU(),
+                 out_function: Callable = nn.Sigmoid()):
         super().__init__()
-        self.encoder = nn.Sequential(*_init_modules(hidden_sizes, activation_functions, reverse=False))
-        self.decoder = nn.Sequential(*_init_modules(hidden_sizes, activation_functions, reverse=True))
-        self.latent_size = hidden_sizes[-1]
+        self.encoder_hidden_sizes = []
+        self.decoder_hidden_sizes = []
+        self.activation_function = activation_function
+        self.out_function = out_function
+        self.init_sizes(hidden_sizes)
+        self.encoder = nn.Sequential(*self.init_module(self.encoder_hidden_sizes, self.activation_function,
+                                                       self.out_function, reverse=False))
+        self.decoder = nn.Sequential(*self.init_module(self.decoder_hidden_sizes, self.activation_function,
+                                                       self.out_function, reverse=True))
 
+    def init_sizes(self, hidden_sizes):
+        self.encoder_hidden_sizes = copy.deepcopy(hidden_sizes)
+        self.decoder_hidden_sizes = copy.deepcopy(hidden_sizes)
+
+    def init_module(self, hidden_sizes: Iterable, activation_function: Callable, out_activation: Callable,
+                    reverse: bool):
+        if reverse:
+            hidden_sizes = list(reversed(hidden_sizes))
+        layers = self.init_layers(hidden_sizes, activation_function)
+        if reverse:
+            layers[-1] = out_activation
+        return layers
+
+    @staticmethod
+    def init_layers(hidden_sizes: Iterable, activation_function: Callable):
+        layers = []
+        for i, (l_in, l_out) in enumerate(zip(hidden_sizes, hidden_sizes[1:])):
+            layers.append(nn.Linear(l_in, l_out))
+            layers.append(activation_function)
+
+        return layers
+
+
+class AE(AbstractAE):
     def forward(self, x):
         z = self.encoder(x)
         xHat = self.decoder(z)
@@ -27,17 +50,13 @@ class AE(nn.Module):
         return xHat, z
 
 
-class VAE(nn.Module):
-    def __init__(self, hidden_sizes, activation_function=nn.ReLU):
-        super().__init__()
-        hidden_sizes_input = hidden_sizes
-        hidden_sizes_input[-1] = hidden_sizes_input[-1] * 2
-        self.encoder = nn.Sequential(*_init_modules(hidden_sizes_input, activation_function, reverse=False))
-        self.decoder = nn.Sequential(*_init_modules(hidden_sizes, activation_function, reverse=True))
-        self.latent_size = hidden_sizes[-1]
+class VAE(AbstractAE):
+    def init_sizes(self, hidden_sizes):
+        super().init_sizes(hidden_sizes)
+        self.encoder_hidden_sizes[-1] *= 2
 
     @staticmethod
-    def _reparameterize(mu, logvar):
+    def _reparametrize(mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         z = eps * std + mu
@@ -50,12 +69,3 @@ class VAE(nn.Module):
         xHat = self.decoder(z)
 
         return xHat, mu, logvar
-
-
-class SiameseNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        pass
-
-    def forward(self, x):
-        pass
